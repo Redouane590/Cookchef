bbbeb8eb0ee83a5fa9cb4ef0399b3d341f7ec02f
Redouane Elmahi <redouaneelmahi@gmail.com>
2023-02-28 18:20:23 +0100

    creation de recettes en dur, iterration sur toutes les recette pour les afficher, mise en place du responsive avec des mixin

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,19 +1,18 @@
 import styles from './Content.module.scss'
 import Recipe from './Recipe';
-
+import { data } from "../data/recipes";
 function Content() {
+  const recipes = data;
   return (
     <div className="flex-fill container p-20">
       <h1 className='my-30'>Découvez nos nouvelles recettes</h1>
       <div className={`card p-20 ${styles.cardContent}`}>
         <div className={styles.grid}>
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
+
+          { recipes.map( (r) => (
+          <Recipe title={r.title} image={r.image} key={r._id}/>
+          ))}
+
         </div>
       </div>
     </div>
